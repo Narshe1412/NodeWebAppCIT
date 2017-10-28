dede83b126b9c1c792e26a1532178c2ec769ff13
Manuel <manuel1412@gmail.com>
2017-10-28 15:19:23 +0100

    routes configured

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+//const port = process.env.PORT || 3000;
+const articles = [{title: 'Example'}];
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
+// app.get('/', (req,res) => {
+//     res.send('Hello world');
+// })
+
+// app.listen(port, () => {
+//     console.log(`Express running at port: ${port}`)
+// })
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res, next) => {
+    res.send('Article fetcher')
 })
 
-app.listen(port, () => {
-    console.log(`Express running at port: ${port}`)
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
 })
+
+app.post('/articles', (req, res, next) => {
+    res.send('OK');
+})
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Fetching:', id);
+    res.send(articles[id]);
+})
+
+app.delete('articles/:id', (req, res, next) =>{
+    const id = req.params.id;
+    console.log('Deleting:', id);
+    delete articles[id];
+    res.send({message: 'Deleted'});
+})
+
+app.listen(app.get('port'), () => {
+    console.log('App started on port', app.get('port'));
+})
+
+module.exports = app;
