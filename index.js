317f88c0b72d147f8edc0353e8e7781bd5b2d079
Manuel <manuel1412@gmail.com>
2017-10-29 16:01:29 +0000

    added sql to main

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const express = require('express');
 const app = express();
 const articles = [{title: 'Example'}];
 const bodyParser = require('body-parser');
-
-// app.get('/', (req,res) => {
-//     res.send('Hello world');
-// })
-
-// app.listen(port, () => {
-//     console.log(`Express running at port: ${port}`)
-// })
+const Article = require('./db').Article;
 
 app.set('port', process.env.PORT || 3000);
 
@@ -21,7 +14,10 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/articles', (req, res, next) => {
-    res.send(articles);
+    Article.all((err, articles) => {
+        if(err) return next(err);
+        res.send(articles);
+    })
 })
 
 app.post('/articles', (req, res, next) => {
@@ -32,15 +28,18 @@ app.post('/articles', (req, res, next) => {
 
 app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Fetching:', id);
-    res.send(articles[id]);
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    })
 })
 
 app.delete('articles/:id', (req, res, next) =>{
     const id = req.params.id;
-    console.log('Deleting:', id);
-    delete articles[id];
-    res.send({message: 'Deleted'});
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({message: 'Deleted'});
+    })
 })
 
 app.listen(app.get('port'), () => {
