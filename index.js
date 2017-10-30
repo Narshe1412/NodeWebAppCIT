2736f96cda078611a639ed4661af96e1b535a347
Manuel <manuel1412@gmail.com>
2017-10-30 11:03:18 +0000

    template finished

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ app.get('/', (req, res, next) => {
 app.get('/articles', (req, res, next) => {
     Article.all((err, articles) => {
         if(err) return next(err);
-        res.send(articles);
+        //res.send(articles);
+        res.format({
+            html: () => { res.render('articles.ejs', {articles:articles})},
+            json: () => { res.send(articles)}
+        })
     })
 })
 
